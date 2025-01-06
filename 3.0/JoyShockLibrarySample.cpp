dadf08dd335ceea31159cc13a77a31c6251de584
r57zone <r57zone@gmail.com>
2025-01-06 14:28:04 +0400

    Stick wheel value

--- 3.0/JoyShockLibrarySample.cpp
+++ 3.0/JoyShockLibrarySample.cpp
@@ -1,8 +1,40 @@
-﻿// Based on https://gist.github.com/JibbSmart/8cbaba568c1c2e1193771459aa5385df
-
-#include <iostream>
+﻿#include <iostream>
 #include "JoyShockLibrary.h"
 #include <windows.h>
+
+struct EulerAngles {
+	double Yaw;
+	double Pitch;
+	double Roll;
+};
+
+EulerAngles QuaternionToEulerAngle(double qW, double qX, double qY, double qZ) // https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+{
+	EulerAngles resAngles;
+	// roll (x-axis rotation)
+	double sinr = +2.0 * (qW * qX + qY * qZ);
+	double cosr = +1.0 - 2.0 * (qX * qX + qY * qY);
+	resAngles.Roll = atan2(sinr, cosr);
+
+	// pitch (y-axis rotation)
+	double sinp = +2.0 * (qW * qY - qZ * qX);
+	if (fabs(sinp) >= 1)
+		resAngles.Pitch = copysign(3.14159265358979323846 / 2, sinp); // use 90 degrees if out of range
+	else
+		resAngles.Pitch = asin(sinp);
+
+	// yaw (z-axis rotation)
+	double siny = +2.0 * (qW * qZ + qX * qY);
+	double cosy = +1.0 - 2.0 * (qY * qY + qZ * qZ);
+	resAngles.Yaw = atan2(siny, cosy);
+
+	return resAngles;
+}
+
+double RadToDeg(double Rad)
+{
+	return Rad / 3.14159265358979323846 * 180.0;
+}
 
 // Settings
 int _gyroSpace = 2;
@@ -44,17 +76,53 @@ void MoveMouse(float x, float y)
 	SendInput(1, &input, sizeof(input));
 }
 
-void CalculateYawPitchRoll(float quatW, float quatX, float quatY, float quatZ, float& yaw, float& pitch, float& roll)
+double OffsetYPR2(double Angle1, double Angle2) // CalcMotionStick
 {
-	yaw = atan2f(2.0f * (quatW * quatZ + quatX * quatY), 1.0f - 2.0f * (quatY * quatY + quatZ * quatZ));
-	pitch = asinf(2.0f * (quatW * quatY - quatZ * quatX));
-	roll = atan2f(2.0f * (quatW * quatX + quatY * quatZ), 1.0f - 2.0f * (quatX * quatX + quatY * quatY));
+	Angle1 -= Angle2;
+	if (Angle1 < -3.14159265358979323846)
+		Angle1 += 2 * 3.14159265358979323846;
+	else if (Angle1 > 3.14159265358979323846)
+		Angle1 -= 2 * 3.14159265358979323846;
+	return Angle1;
 }
+
+float CalcMotionStick(float gravA, float gravB, float wheelAngle, float offsetAxis) {
+	float angleRadians = wheelAngle * (3.14159f / 180.0f); // To radians
+
+	float normalizedValue = OffsetYPR2(atan2f(gravA, gravB), offsetAxis) / angleRadians;
+
+	if (normalizedValue > 1.0f)
+		normalizedValue = 1.0f;
+	else if (normalizedValue < -1.0f)
+		normalizedValue = -1.0f;
+
+	return normalizedValue;
+}
+
+float NormalizeHorizontalAngleToStick(float angle, float maxAngle)
+{
+	// Преобразуем угол в диапазон [-1, 1]
+	float normalizedValue = angle / maxAngle;
+
+	// Ограничиваем значение в пределах [-1, 1]
+	if (normalizedValue > 1.0f)
+		normalizedValue = 1.0f;
+	else if (normalizedValue < -1.0f)
+		normalizedValue = -1.0f;
+
+	return normalizedValue;
+}
+
 
 void HandleDeviceConnections()
 {
 	int deviceHandles[16];
 	int numConnectedDevices = JslGetConnectedDeviceHandles(deviceHandles, 16);
+
+	const float maxAngleDegrees = 150.0f; // Wheel max angle
+
+	float OffsetAxisX = 0;
+	EulerAngles MotionAngles;
 
 	if (numConnectedDevices > 0)
 	{
@@ -76,16 +144,18 @@ void HandleDeviceConnections()
 
 		
 			MOTION_STATE motionState = JslGetMotionState(deviceHandle);
-			float yaw, pitch, roll;
-			CalculateYawPitchRoll(motionState.quatW, motionState.quatX, motionState.quatY, motionState.quatZ, yaw, pitch, roll);
+			MotionAngles = QuaternionToEulerAngle(motionState.quatW, motionState.quatZ, motionState.quatX, motionState.quatY);
 
-			//std::cout << "Device " << deviceIndex << " connected." << std::endl;
-			//std::cout << "Gravity = (" << motionState.gravX << ", " << motionState.gravY << ", " << motionState.gravZ << ")" << std::endl;
-			//std::cout << "Acceleration = (" << motionState.accelX << ", " << motionState.accelY << ", " << motionState.accelZ << ")" << std::endl;
+			float WheelAngle = 150.0f;
+
 			
-			//std::cout << "Yaw = " << yaw << ", Pitch = " << pitch << ", Roll = " << roll << std::endl; // Перестает работать влево мышки ???
+			float stickValue = CalcMotionStick(motionState.gravX, motionState.gravZ, maxAngleDegrees, OffsetAxisX);
+
+			//std::cout << "Device " << deviceIndex  std::endl;
+
+			std::cout << "Wheel roll value: " << stickValue * 100 << "% " << "Yaw = " << RadToDeg(MotionAngles.Yaw) << ", Pitch = " << RadToDeg(MotionAngles.Pitch) << ", Roll = " << RadToDeg(MotionAngles.Roll) << std::endl; // Перестает работать влево мышки ???
 		
-			std::cout << "Devices connected: " << deviceIndex << std::endl;
+			//std::cout << "Devices connected: " << deviceIndex << std::endl;
 		}
 	}
 	else
